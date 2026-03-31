f3f13dba8417358db581140139302b454841a97f
Dmitrii Shushin <ds.shushin@gmail.com>
2026-03-31 14:32:20 +0300

    feat: add image filter appliance

--- js/upload-form.js
+++ js/upload-form.js
@@ -3,15 +3,79 @@ import {isEscapeKey, checkIfDuplicateExists} from './util.js';
 const MAX_HASHTAG_COUNT = 5;
 const MAX_COMMENT_LENGTH = 140;
 
+const IMAGE_SCALE_STEP = 25;
+const MIN_IMAGE_SCALE = 25;
+const MAX_IMAGE_SCALE = 100;
+
 const uploadForm = document.querySelector('.img-upload__form');
 const uploadFormOverlay = uploadForm.querySelector('.img-upload__overlay');
 const uploadFormCancel = uploadForm.querySelector('.img-upload__cancel');
 
 const imgInput = uploadForm.querySelector('.img-upload__input');
+const imgPreview = uploadForm.querySelector('.img-upload__preview>img');
+const imgUploadEffects = uploadForm.querySelector('.img-upload__effects');
+
 const hashtagInput = uploadForm.querySelector('.text__hashtags');
 const descriptionInput = uploadForm.querySelector('.text__description');
 
+const effectLevel = uploadForm.querySelector('.img-upload__effect-level');
+const effectLevelSlider = effectLevel.querySelector('.effect-level__slider');
+const effectLevelValue = effectLevel.querySelector('.effect-level__value');
+
+const scaleValue = uploadForm.querySelector('.scale__control--value');
+const scaleSmallerButton = uploadForm.querySelector('.scale__control--smaller');
+const scaleBiggerButton = uploadForm.querySelector('.scale__control--bigger');
+
 let hashtagErrorMessage = '';
+let currentImageScale = MAX_IMAGE_SCALE;
+let selectedEffect = 'none';
+effectLevel.classList.add('hidden');
+
+const Filters = {
+  chrome: {
+    effect: 'grayscale',
+    minValue: 0,
+    maxValue: 1,
+    step: 0.1
+  },
+  sepia: {
+    effect: 'sepia',
+    minValue: 0,
+    maxValue: 1,
+    step: 0.1
+  },
+  marvin: {
+    effect: 'invert',
+    minValue: 0,
+    maxValue: 100,
+    step: 1,
+    measure: '%'
+  },
+  phobos: {
+    effect: 'blur',
+    minValue: 0,
+    maxValue: 3,
+    step: 0.1,
+    measure: 'px'
+  },
+  heat: {
+    effect: 'brightness',
+    minValue: 1,
+    maxValue: 3,
+    step: 0.1
+  }
+};
+
+
+noUiSlider.create(effectLevelSlider, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 0,
+  step: 1,
+  connect: 'lower',
+});
 
 
 const pristine = new Pristine(uploadForm, {
@@ -67,6 +131,29 @@ const toggleFormModal = () => {
 };
 
 
+const changeImageScale = (newScale) => {
+  currentImageScale = newScale;
+  scaleValue.value = `${newScale}%`;
+  imgPreview.style.transform = `scale(${newScale / 100})`;
+};
+
+
+const resetImageFilter = () => {
+  effectLevelValue.value = '';
+  imgPreview.style.filter = '';
+  effectLevel.classList.add('hidden');
+};
+
+
+const changeImageFilter = (filterObject, newValue) => {
+  effectLevel.classList.remove('hidden');
+
+  const filterValue = `${newValue}${filterObject.measure ?? ''}`;
+  imgPreview.style.filter = `${filterObject.effect}(${filterValue})`;
+  effectLevelValue.value = filterValue;
+};
+
+
 /**
  * Сбрасывает значения всех полей формы загрузки изображения
  */
@@ -74,6 +161,9 @@ const clearFormInputs = () => {
   imgInput.value = '';
   hashtagInput.value = '';
   descriptionInput.value = '';
+
+  changeImageScale(MAX_IMAGE_SCALE);
+  resetImageFilter();
 
   pristine.reset(); // Сбрасываем состояние Pristine
 };
@@ -147,3 +237,33 @@ imgInput.addEventListener('input', () => openUploadForm());
 uploadFormCancel.addEventListener('click', () => closeUploadForm());
 
 uploadForm.addEventListener('submit', onUploadFormSubmit);
+
+
+scaleSmallerButton.addEventListener('click', () => {
+  changeImageScale(Math.max(MIN_IMAGE_SCALE, currentImageScale - IMAGE_SCALE_STEP));
+});
+scaleBiggerButton.addEventListener('click', () => {
+  changeImageScale(Math.min(MAX_IMAGE_SCALE, currentImageScale + IMAGE_SCALE_STEP));
+});
+
+imgUploadEffects.addEventListener('input', () => {
+  selectedEffect = document.querySelector('input[name=effect]:checked').value;
+  if (Object.hasOwn(Filters, selectedEffect)) {
+    effectLevelSlider.noUiSlider.updateOptions({
+      range: {
+        min: Filters[selectedEffect].minValue,
+        max: Filters[selectedEffect].maxValue
+      },
+      start: Filters[selectedEffect].maxValue,
+      step: Filters[selectedEffect].step
+    });
+  } else {
+    resetImageFilter();
+  }
+});
+
+effectLevelSlider.noUiSlider.on('update', () => {
+  if (Object.hasOwn(Filters, selectedEffect)) {
+    changeImageFilter(Filters[selectedEffect], effectLevelSlider.noUiSlider.get());
+  }
+});
