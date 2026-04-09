2a892563b6bf67bfe09289fb5be448fcd8cc02d8
Dmitrii Shushin <ds.shushin@gmail.com>
2026-04-09 11:08:04 +0300

    feat: add error message for data getter

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-import {renderPictures} from './pictures.js';
 import {getData} from './api.js';
+import {showDataError} from './data-error.js';
+import {renderPictures} from './pictures.js';
 import './upload-form.js';
 
 getData()
-  .then((photos) => renderPictures(photos));
+  .then((photos) => renderPictures(photos))
+  .catch((err) => showDataError(err.message));
